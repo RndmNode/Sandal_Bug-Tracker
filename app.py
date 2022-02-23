b74ca0e2a48599e6b5737d3cb601ba5e209f90ce
Taylor Zeller <whitleyzeller@gmail.com>
2022-02-22 22:00:13 -0700

    Added home page, still needs frontend design/touchups

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
+from crypt import methods
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/login')
+@app.route('/', methods=['GET'])
 def home():
+    return render_template('home.html')
+
+@app.route('/login', methods=['GET'])
+def login():
     return render_template('login.html')
 
 def run():
-    app.run(debug=True)
+    app.run(debug=True, port='8000')
 
 if __name__ == "__main__":
     run()
