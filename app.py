139872667ab1891979db1abbd233ca9039f052a8
Taylor Zeller <whitleyzeller@gmail.com>
2022-02-26 18:58:42 -0700

    Fixed user forms, can now see input as well. Added links to each page and alerts for input validation

--- app.py
+++ app.py
@@ -1,20 +1,35 @@
 from crypt import methods
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'ef3ef11521a594a6bb4573fb466ba31f'
 
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('home.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'tz@g.com' and form.password.data == 'haha':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful. Please check username and password.', 'danger')
+        
+    return render_template('login.html', form=form)
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return render_template('signup.html')
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('signup.html', form=form)
 
 def run():
     app.run(debug=True, port='8000')
