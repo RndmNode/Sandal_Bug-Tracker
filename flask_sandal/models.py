7688b3792e86985a005d64f007b283c2b137da70
Taylor Zeller <whitleyzeller@gmail.com>
2022-03-06 17:47:07 -0700

    added updates to issues to the database

--- flask_sandal/models.py
+++ flask_sandal/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from email.policy import default
 from flask_sandal import db
 
 user_project = db.Table('user_project',
@@ -12,6 +13,7 @@ class User(db.Model):
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
+
     # relationships
     projects = db.relationship('Project', secondary=user_project, backref='team')
 
@@ -22,6 +24,7 @@ class Project(db.Model):
     # columns
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(60), nullable=False)
+
     # relationships
     issues = db.relationship('Issue', backref='project', lazy=True)
 
@@ -31,12 +34,27 @@ class Project(db.Model):
 class Issue(db.Model):
     # columns
     ticket_number = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(60), nullable=False)
+    description = db.Column(db.String(60), nullable=False)
     priority = db.Column(db.String(15), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     details = db.Column(db.Text, nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    # states and their progression levels in order: ['New', 'Open', 'Pending', 'Resolved', 'Closed']
+    state = db.Column(db.Text, nullable=False, default='New')
+
     # relationships
     project_id = db.Column(db.Integer, db.ForeignKey('project.id'), nullable=False)
 
     def __repr__(self):
-        return f"Issue('{self.ticket_number}, {self.title}, {self.priority}, {self.date_posted}, {self.details}')"
+        return f"Issue('{self.ticket_number}, {self.description}, {self.priority}, {self.date_posted}, {self.details}')"
+
+class Update(db.Model):
+    # columns
+    id = db.Column(db.Integer, primary_key=True)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    details = db.Column(db.Text, nullable=False, default="")
+
+    # relationships
+    issue_id = db.Column(db.Integer, db.ForeignKey('issue.ticket_number'), nullable=False)
+
+    def __repr__(self):
+        return f"Update('{self.id}, {self.details}, {self.date_posted}')"
